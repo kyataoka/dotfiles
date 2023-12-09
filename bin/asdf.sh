2769ca15a0156d74e2ac029a7cd4cb4d0cf66be0
kataokahiroki <kyataoka.deve@gmail.com>
2023-12-09 19:46:20 +0900

    update flutter versions

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -5,7 +5,9 @@ asdf install nodejs lts
 asdf global nodejs lts
 
 asdf plugin add flutter
-asdf install flutter 3.10.5-stable
-asdf install flutter 3.7.4-stable
-asdf install flutter 3.3.1-stable
-asdf global flutter 3.10.5-stable
+asdf install flutter 3.16.3-stable
+asdf install flutter 3.13.9-stable
+asdf install flutter 3.10.6-stable
+asdf install flutter 3.7.12-stable
+asdf install flutter 3.3.10-stable
+asdf global flutter 3.16.3-stable
