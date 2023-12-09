c6ad5aa990321e0550bcdec1bbe37804b609c195
kataokahiroki <kyataoka.deve@gmail.com>
2023-12-10 01:15:34 +0900

    Add Xcode command line tools and Rosetta 2 installation, Prezto and Homebrew setup

--- bin/init.sh
+++ bin/init.sh
@@ -1,20 +1,26 @@
 #!/bin/zsh
 
+# Install Xcode command line tools
 xcode-select --install
 
-# Install rosetta2
+# Install Rosetta 2 for Apple Silicon
 if [ "$(uname -m)" = "arm64" ]; then
   softwareupdate --install-rosetta --agree-to-license
 fi
 
 # Install Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+fi
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/kataokahiroki/.zprofile
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "${ZDOTDIR:-$HOME}/.zprofile"
+fi
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
