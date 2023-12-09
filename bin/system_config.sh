00aa82031e3ce5f493e162c7c7ddd39ca79a30d1
kataokahiroki <kyataoka.deve@gmail.com>
2023-12-10 03:24:27 +0900

    Update system_config.sh: Remove default apps from dock, set dock size, hide app suggestions, and restart dock

--- bin/system_config.sh
+++ bin/system_config.sh
@@ -16,4 +16,16 @@ cd "$ROOT_DIR"
 # Enable key press and hold
 defaults write -g ApplePressAndHoldEnabled -bool true
 
+# Remove all default apps from dock
+defaults write com.apple.dock persistent-apps -array
+
+# Set dock size
+defaults write com.apple.dock tilesize -int 36
+
+# Hide App Suggestions
+defaults write com.apple.dock show-recents -bool false
+
+# Restart Dock
+killall Dock
+
 rm -rf "$TMP_DIR"
