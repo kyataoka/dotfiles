c07f69e63d6e067e3557efccdd104e9b30408dcb
kataokahiroki <kyataoka.deve@gmail.com>
2023-12-11 01:13:46 +0900

    Update nodejs and flutter versions

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
+set -e
+
+sudo xcodebuild -license
+
 asdf plugin add nodejs
 asdf install nodejs lts
 asdf global nodejs lts
 
 asdf plugin add flutter
-asdf install flutter 3.16.3-stable
-asdf install flutter 3.13.9-stable
-asdf install flutter 3.10.6-stable
-asdf install flutter 3.7.12-stable
-asdf install flutter 3.3.10-stable
-asdf global flutter 3.16.3-stable
+asdf install flutter $(asdf latest flutter)
+asdf global flutter $(asdf latest flutter)
