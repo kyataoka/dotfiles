4e7f399fe41381fd45bdc831ec73a2621365d452
kyataoka <kyataoka.deve@gmail.com>
2024-04-20 19:01:24 +0900

    Update Dock configuration and dependencies

--- bin/system_config.sh
+++ bin/system_config.sh
@@ -16,8 +16,53 @@ cd "$ROOT_DIR"
 # Enable key press and hold
 defaults write -g ApplePressAndHoldEnabled -bool true
 
-# Remove all default apps from dock
-defaults write com.apple.dock persistent-apps -array
+# Setup Dock
+dockutil --remove all --no-restart
+# Add applications to dock
+applications=(
+  "/Applications/Finder.app"
+  "/Applications/Launchpad.app"
+  "/Applications/Mission Control.app"
+  "/System/Applications/Home.app"
+  "/Applications/Google Chrome.app"
+  "/Applications/Safari.app"
+  "/Applications/Discord.app"
+  "/System/Applications/Messages.app"
+  "/System/Applications/Mail.app"
+  "/System/Applications/Maps.app"
+  "/System/Applications/Photos.app"
+  "/System/Applications/Calendar.app"
+  "/System/Applications/Notes.app"
+  "/System/Applications/Reminders.app"
+  "/System/Applications/Music.app"
+  "/System/Applications/App Store.app"
+  "/System/Applications/Utilities/Terminal.app"
+  "/System/Applications/System Settings.app"
+  "/Applications/Visual Studio Code.app"
+  "/Applications/Xcode.app"
+  "/Applications/Parallels Desktop.app"
+  "/Applications/Microsoft Remote Desktop.app"
+  "/Applications/RemotePlay.app"
+  "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+  "/Applications/Unity Hub.app"
+  "/Applications/Minecraft.app"
+  "/Applications/1Password.app"
+  "/System/Applications/Utilities/Screen Sharing.app"
+  "/Applications/AppCleaner.app"
+)
+position=1
+for app in "${applications[@]}"; do
+  # if app exists add it to the dock
+  if [ -d "$app" ]; then
+    dockutil --add "$app" --position $position --section apps --no-restart > /dev/null
+    position=$((position + 1))
+  fi
+done
+# Add directories to dock
+dockutil --add "$HOME/Downloads" --position 1 --section others --view fan --display stack --sort dateadded --no-restart > /dev/null
+dockutil --add "$HOME/Documents/screenshot" --position 2 --section others --view grid --display stack --sort dateadded --no-restart > /dev/null
+# Restart Dock
+killall Dock
 
 # Set dock size
 defaults write com.apple.dock tilesize -int 36
