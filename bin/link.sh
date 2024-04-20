bced4f10b9e9b040b7c71edc6aba7cfa4f9d6518
kyataoka <kyataoka.deve@gmail.com>
2024-04-20 23:40:00 +0900

    Change config files

--- bin/link.sh
+++ bin/link.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 DOTFILES_DIR="$(cd "$SCRIPT_DIR"/.. && pwd)"
 
-ln -sf "$DOTFILES_DIR"/vimrc ~/.vimrc
-ln -sf "$DOTFILES_DIR"/tmux.conf ~/.tmux.conf
-ln -sf "$DOTFILES_DIR"/zshrc ~/.zshrc
-ln -sf "$DOTFILES_DIR"/zprezto ~/.zprezto
-ln -sf "$DOTFILES_DIR"/git/gitconfig ~/.gitconfig
+ln -sf "$DOTFILES_DIR"/files/vimrc ~/.vimrc
+ln -sf "$DOTFILES_DIR"/files/tmux.conf ~/.tmux.conf
+ln -sf "$DOTFILES_DIR"/files/zshrc ~/.zshrc
+ln -sf "$DOTFILES_DIR"/files/zprezto ~/.zprezto
+ln -sf "$DOTFILES_DIR"/files/git/gitconfig ~/.gitconfig
