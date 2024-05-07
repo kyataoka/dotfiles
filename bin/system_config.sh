08ec21725adca673acfe27f181d62232e2d8b292
kyataoka <kyataoka.deve@gmail.com>
2024-05-07 10:48:36 +0900

    Update system_config.sh to configure dock settings

--- bin/system_config.sh
+++ bin/system_config.sh
@@ -27,81 +27,6 @@ if [ "$IS_DEFAULTBROWSER_INSTALLED" = false ]; then
 fi
 
 ###############################################################################
-# Dock settings
-###############################################################################
-
-# if dockutil is not installed, install it. and uninstall after use
-if ! command -v dockutil &> /dev/null; then
-  IS_DOCKUTIL_INSTALLED=false
-  brew install dockutil
-else
-  IS_DOCKUTIL_INSTALLED=true
-fi
-
-dockutil --remove all --no-restart
-# Add applications to dock
-applications=(
-  "/Applications/Finder.app"
-  "/Applications/Launchpad.app"
-  "/Applications/Mission Control.app"
-  "/System/Applications/Home.app"
-  "/Applications/Google Chrome.app"
-  "/Applications/Safari.app"
-  "/Applications/Discord.app"
-  "/System/Applications/Messages.app"
-  "/System/Applications/Mail.app"
-  "/System/Applications/Maps.app"
-  "/System/Applications/Photos.app"
-  "/System/Applications/Calendar.app"
-  "/System/Applications/Notes.app"
-  "/System/Applications/Reminders.app"
-  "/System/Applications/Music.app"
-  "/System/Applications/App Store.app"
-  "/System/Applications/Utilities/Terminal.app"
-  "/System/Applications/System Settings.app"
-  "/Applications/Visual Studio Code.app"
-  "/Applications/Xcode.app"
-  "/Applications/Parallels Desktop.app"
-  "/Applications/Microsoft Remote Desktop.app"
-  "/Applications/RemotePlay.app"
-  "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-  "/Applications/Unity Hub.app"
-  "/Applications/Minecraft.app"
-  "/Applications/1Password.app"
-  "/System/Applications/Utilities/Screen Sharing.app"
-  "/Applications/AppCleaner.app"
-)
-position=1
-for app in "${applications[@]}"; do
-  # if app exists add it to the dock
-  if [ -d "$app" ]; then
-    dockutil --add "$app" --position $position --section apps --no-restart > /dev/null
-    position=$((position + 1))
-  fi
-done
-
-# Add directories to dock
-dockutil --add "$HOME/Downloads" --position 1 --section others --view fan --display stack --sort dateadded --no-restart > /dev/null
-dockutil --add "$HOME/Documents/screenshot" --position 2 --section others --view grid --display stack --sort dateadded --no-restart > /dev/null
-
-# Uninstall dockutil if it was not installed
-if [ "$IS_DOCKUTIL_INSTALLED" = false ]; then
-  brew uninstall dockutil
-fi
-
-# Set dock size
-defaults write com.apple.dock tilesize -int 36
-
-# Hide App Suggestions
-defaults write com.apple.dock show-recents -bool false
-
-# Disable auto rearrange spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
-# Restart Dock
-killall Dock
-
-###############################################################################
 # Sound settings
 ###############################################################################
 
@@ -196,6 +121,89 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 # Set Finder to desktop group by kind
 defaults write com.apple.finder DesktopViewSettings -dict GroupBy "Kind"
+
+# Enable sync of desktop and documents to iCloud
+defaults write com.apple.finder FXICloudDriveEnabled -bool true
+defaults write com.apple.finder FXICloudDriveDesktop -bool true
+defaults write com.apple.finder FXICloudDriveDocuments -bool true
+
+# kill Finder
+killall Finder
+
+###############################################################################
+# Dock settings
+###############################################################################
+
+# if dockutil is not installed, install it. and uninstall after use
+if ! command -v dockutil &> /dev/null; then
+  IS_DOCKUTIL_INSTALLED=false
+  brew install dockutil
+else
+  IS_DOCKUTIL_INSTALLED=true
+fi
+
+dockutil --remove all --no-restart
+# Add applications to dock
+applications=(
+  "/Applications/Finder.app"
+  "/Applications/Launchpad.app"
+  "/Applications/Mission Control.app"
+  "/System/Applications/Home.app"
+  "/Applications/Google Chrome.app"
+  "/Applications/Safari.app"
+  "/Applications/Discord.app"
+  "/System/Applications/Messages.app"
+  "/System/Applications/Mail.app"
+  "/System/Applications/Maps.app"
+  "/System/Applications/Photos.app"
+  "/System/Applications/Calendar.app"
+  "/System/Applications/Notes.app"
+  "/System/Applications/Reminders.app"
+  "/System/Applications/Music.app"
+  "/System/Applications/App Store.app"
+  "/System/Applications/Utilities/Terminal.app"
+  "/System/Applications/System Settings.app"
+  "/Applications/Visual Studio Code.app"
+  "/Applications/Xcode.app"
+  "/Applications/Parallels Desktop.app"
+  "/Applications/Microsoft Remote Desktop.app"
+  "/Applications/RemotePlay.app"
+  "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+  "/Applications/Unity Hub.app"
+  "/Applications/Minecraft.app"
+  "/Applications/1Password.app"
+  "/System/Applications/Utilities/Screen Sharing.app"
+  "/Applications/AppCleaner.app"
+)
+position=1
+for app in "${applications[@]}"; do
+  # if app exists add it to the dock
+  if [ -d "$app" ]; then
+    dockutil --add "$app" --position $position --section apps --no-restart > /dev/null
+    position=$((position + 1))
+  fi
+done
+
+# Add directories to dock
+dockutil --add "$HOME/Downloads" --position 1 --section others --view fan --display stack --sort dateadded --no-restart > /dev/null
+dockutil --add "$HOME/Documents/screenshot" --position 2 --section others --view grid --display stack --sort dateadded --no-restart > /dev/null
+
+# Uninstall dockutil if it was not installed
+if [ "$IS_DOCKUTIL_INSTALLED" = false ]; then
+  brew uninstall dockutil
+fi
+
+# Set dock size
+defaults write com.apple.dock tilesize -int 36
+
+# Hide App Suggestions
+defaults write com.apple.dock show-recents -bool false
+
+# Disable auto rearrange spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Restart Dock
+killall Dock
 
 ###############################################################################
 # Sleep settings
