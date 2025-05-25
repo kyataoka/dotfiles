d3c9ca95ba6c3accbcc4ae10f241126e1645d612
kyataoka <kyataoka.deve@gmail.com>
2025-05-25 22:27:06 +0900

    Refactor asdf.sh to use 'asdf set --home' for version management and add .DS_Store to gitignore; update zshrc for golang and pnpm paths

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -9,9 +9,14 @@ source ~/.zshrc || true
 # Install nodejs
 asdf plugin add nodejs
 asdf install nodejs $(asdf latest nodejs)
-asdf global nodejs $(asdf latest nodejs)
+asdf set --home nodejs $(asdf latest nodejs)
 
 # Install flutter
 asdf plugin add flutter
 asdf install flutter $(asdf latest flutter)
-asdf global flutter $(asdf latest flutter)
+asdf set --home flutter $(asdf latest flutter)
+
+# Install golang
+asdf plugin add golang
+asdf install golang $(asdf latest golang)
+asdf set --home golang $(asdf latest golang)
